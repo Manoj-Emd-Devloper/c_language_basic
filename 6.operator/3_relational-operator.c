e06b2cd44c4f73809609f62f2a223aded1872a94
manoj <patilmanoj9528@gmail.com>
2024-01-22 16:50:37 +0530

    modified program 3_relational-operator.c

--- 6.operator/3_relational-operator.c
+++ 6.operator/3_relational-operator.c
@@ -1,8 +1,4 @@
 #include <stdio.h>
-void equal(int c, int d);
-void compare1(int e, int f);
-void comapare2(int g,int h);
-void compare3(int i,int j);
 
 int main() {
     int a,b;
@@ -11,52 +7,40 @@ int main() {
     printf("Enter the value of b=");
     scanf("%d",&b);
 
-    equal(a,b);
-    compare1(a,b);
-    comapare2(a,b);
-    compare3(a,b);
-    getchar();
-    return 0;
-}
-
-void equal(int c, int d){
-    if(c==d){
+    if(a==b){
         printf("a is equal to b \n");
     }
-    else if(c!=d){
+    else if(a!=b){
         printf("a is not equal to b \n");
     }
     else{
         printf("no relation a and b \n");
     }
-}
 
-void compare1(int e, int f){
-    if(e<f){
+    if(a<b){
         printf("a are less than b \n");
     }
-    else if(e>f){
+    else if(a>b){
         printf("a are greter than b \n");
     }
     else{
         printf("no greater and less than relation between a and b \n");
     }
-}
 
-void comapare2(int g,int h){
-    if(g<=h){
-        printf("a are less than or equal to b");
+    if(a<=b){
+        printf("a are less than or equal to b \n");
     }
     else{
         printf("no comapare and equal to relation between a and d \n");
     }
-}
 
-void compare3(int i,int j){
-    if(i>=j){
-        printf("a are greater than or equal to b");
+    if(a>=b){
+        printf("a are greater than or equal to b \n");
     }
     else{
         printf("no comapare and equal to relation between a and d \n");
     }
+
+    getchar();
+    return 0;
 }
--- 6.operator/distance.c
+++ 6.operator/distance.c
@@ -3,6 +3,6 @@ int main(){
     unsigned char distanceA2B=160,distanceB2C=40;
     unsigned char distanceA2C;
     distanceA2C=distanceA2B+distanceB2C;
-    printf("Total distance from A2C = %o Km\n",distanceA2C);
+    printf("Total distance from A2C = %u Km\n",distanceA2C);
     return 0;
 }

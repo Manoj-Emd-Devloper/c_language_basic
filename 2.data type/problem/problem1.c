7931060dec299a119ba15ec452d9b3583674c18e
manoj <patilmanoj9528@gmail.com>
2024-02-29 13:06:44 +0530

    modify problem

--- 2.data type/problem/problem1.c
+++ 2.data type/problem/problem1.c
@@ -2,11 +2,14 @@
 
 int main() {
     char c = 255 ;
+    printf("c = %d \n", c);
     c = c + 10;
     printf("c = %d \n", c);
     return 0;
 }
 //answer is 9
-//unsigned char or char can store 1byte of data range from (0 to 255) or (-128 to 127). if we add +10 then 255 + 10 = 265
-//the value is execeeding and start from 0
-//also calculate by mod 2^8 =256 (^8 is 8 bit) , 265%256 =9 
+//unsigned char or char can store 1byte of data range from (0 to 255) or (-128 to 127).
+//the value is execeeding and c = -1 after c = -1 + 10 = 9
+
+//also calculate by mod m= 2^8 =256 (^8 is 8 bit) , c = 255 + 10 = 265
+//c%m = 265%256 = 9 

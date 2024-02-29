e65663b13b73a631926f091b7b82bef8047312ce
manoj <patilmanoj9528@gmail.com>
2024-02-29 12:59:01 +0530

    modify problem

--- 2.data type/problem/problem1.c
+++ 2.data type/problem/problem1.c
@@ -7,6 +7,6 @@ int main() {
     return 0;
 }
 //answer is 9
-//unsigned char or char can store 1byte of data range from 0 to 255. if we add +10 then 255 + 10 = 265
+//unsigned char or char can store 1byte of data range from (0 to 255) or (-128 to 127). if we add +10 then 255 + 10 = 265
 //the value is execeeding and start from 0
 //also calculate by mod 2^8 =256 (^8 is 8 bit) , 265%256 =9 
